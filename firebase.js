657a93c11b567972b475e577dd617806e4b3b749
Jatin Goyal <25JG2001@GMAIL.COM>
2022-06-19 01:53:20 +0530

    first commit

---
+++ firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDjehWyPboEerHBy5NIfDs6TxPdj1AWk94",
+  authDomain: "whatsapp-eca49.firebaseapp.com",
+  projectId: "whatsapp-eca49",
+  storageBucket: "whatsapp-eca49.appspot.com",
+  messagingSenderId: "1094555325009",
+  appId: "1:1094555325009:web:3a67448cfb7f6d12245e70",
+};
+
+// Initialize Firebase
+const apps = getApps();
+const app = !apps.length ? initializeApp(firebaseConfig) : getApp();
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
